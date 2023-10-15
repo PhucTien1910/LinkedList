7f7b01928b9c2733557f548a92f45862dd9258ba
Phúc Tiền Nguyễn <phuctien.nguyen2004@gmail.com>
2023-10-15 23:15:42 +0700

    đã xong all bài trừ InsertAt(int x, int i)

--- DSLKDon/DSLKDon/MyList.cs
+++ DSLKDon/DSLKDon/MyList.cs
@@ -175,53 +175,137 @@ namespace DSLKDon
         //Trả về danh sách chỉ chứa số chẵn
         public MyList GetEvenList()
         {
-            MyList evenlist = new MyList();
+            MyList evenlist = new MyList(); //khởi tạo 1 MyList(danh sách) rỗng
             IntNode p = first;
 
             while(p != null)
             {
-                if(p.Data % 2 == 0)
-                    evenlist.AddFirst(new IntNode(p.Data));
+                if(p.Data % 2 == 0) //nếu p chia hết cho 2
+                    evenlist.AddFirst(new IntNode(p.Data)); //thêm một node mới vào đầu danh sách(số chẵn)
                 p = p.Next;
             }
-            return evenlist;
+            return evenlist; //trả về danh sách số chẵn
         }
 
         //Trả về danh sách chỉ chứa số lẻ
         public MyList GetOddList()
         {
-            MyList oddlist = new MyList();
+            MyList oddlist = new MyList(); //khởi tạo 1 MyList(danh sách) rỗng
             IntNode p = first;
 
             while(p != null)
             {
-                if (p.Data % 2 != 0)
-                    oddlist.AddFirst(new IntNode(p.Data));
+                if (p.Data % 2 != 0) //nếu p không chia hết cho 2
+                    oddlist.AddFirst(new IntNode(p.Data)); //thêm một node mới vào đầu danh sách (số lẻ)
                 p = p.Next;
             }
-            return oddlist;
+            return oddlist; //trả về danh sách số lẻ
         }
 
-        //chèn x vào sau node có giá trị nhỏ nhất
+        //VD:
+        //5 -> 3 -> 7 -> 9
+        //1. Đi tìm xem có node đó không? 
+        //2. Chỉnh Next của node trước thành node sau node cần xóa
+
+        //1.Xóa node đầu
+        public void DeleteFirst()
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+            //5 -> 3 -> 7 -> 9
+            //create node cần xóa 
+            IntNode pDel = first;
+            //Cập nhật lại FIRST là node sau của node hiện tại
+            first = pDel.Next;
+
+            //TH danh sách chỉ có 1 phần tử (VD: 5)
+            if(first == null)
+            {
+                last = null;
+            }
+            pDel = null;
+        }
+        //2.Xóa node cuối
+        public void DeleteLast()
+        {
+            //5 -> 3 -> 7 -> 9
+            IntNode pDel = last;
+            //2. Chỉnh Next của node trước(pTruoc) thành node sau node cần XÓA(pDel) (VD: xóa 9 => ... -> 7 -> null)
+            IntNode pTruoc = SearchPrevious(pDel);
+            if(pTruoc == null)
+            {
+                first = last = null;    
+            }
+            else
+            {
+                //chỉnh next của node trước là null
+                pTruoc.Next = null;
+                //cập nhật lại giá trị last
+                last = pTruoc;
+            }
+            pDel = null;
+        }
+        //3.Xóa node ở vị trí bất kì
+        public void RemoveX(int x) //truyền vào giá trị cần xóa
+        {           
+            //1. Đi tìm xem có node đó không? 
+            IntNode pDel = SearchX(x);
+            //Node đầu
+            if (pDel == first)
+            {
+                DeleteFirst();
+                return;
+            }
+            //Node cuối
+            if (pDel == last)
+            {
+                DeleteLast();
+                return;
+            }
+            //2. Chỉnh Next của node trước thành node sau node cần xóa
+            //5 -> 3 -> 7 -> 9
+            //5 -> 3 -> 9
+            IntNode pTruoc = SearchPrevious(pDel);
+            pTruoc.Next = pDel.Next;
+            pDel = null;
+        }
+
+        //Tìm node TRƯỚC(pPrev) của node cần XÓA(p) 
+        public IntNode SearchPrevious(IntNode p)//7
+        {
+            if(IsEmpty())
+            {
+                return null;
+            }
+            if(p==first)
+            {
+                return null;
+            }
+            //5 -> 3 -> 7 -> 9
+            //VD xóa node 7: 5 -> 3 -> 9
+            IntNode pPrev = first; //đặt con trỏ đến node đầu tiên
+            while (pPrev.Next != p) 
+            {
+                pPrev = pPrev.Next; //di chuyển đến node tiếp theo và tiếp tục xét 
+            }
+            return pPrev; 
+        }        
+      
+        //hàm thêm vào sau node p 
         public void InsertAfterP(IntNode p, IntNode pNew)
         {
             if(p == last)
-                AddLast(pNew);
+                AddLast(pNew); 
             else
             {
                 pNew.Next = p.Next;
                 p.Next = pNew;
             }
         }
-        public void InsertAfterMin(int min)
-        {           
-            IntNode pMin = GetMin();
-            IntNode pNew = new IntNode(min);
-            InsertAfterP(pMin, pNew);
-        }
-
-        //chèn x vào trước node có giá trị lớn nhất
-        public void HoanVi(IntNode a, IntNode b) 
+        //hàm hoán vị và thêm vào trước node p
+        public void HoanVi(IntNode a, IntNode b)
         {
             int temp = a.Data;
             a.Data = b.Data;
@@ -232,6 +316,36 @@ namespace DSLKDon
             InsertAfterP(p, pNew);
             HoanVi(p, pNew);
         }
+
+        //chèn X vào sau node có giá trị nhỏ nhất
+        public void InsertAfterMin(int min)
+        {           
+            IntNode pMin = GetMin();
+            IntNode pNew = new IntNode(min);
+            InsertAfterP(pMin, pNew);
+        }
+       
+        //chèn X vào sau Y
+        public void InsertXAfterY(int x, int y)
+        {
+            //5 -> 3 -> 7 -> 9
+            //chèn giá trị x = 10 vào sau y = 7
+            //5 -> 3 -> 7 -> (10) -> 9
+            IntNode pX = new IntNode(x);
+            IntNode p = first;
+            while (p != null)
+            {
+                if(p.Data == y)
+                {
+                    InsertAfterP(p, pX);
+                    return;                   
+                }
+                p = p.Next;
+            }
+            Console.WriteLine("Khong tim thay node co gia tri y !");
+        }
+        
+        //chèn x vào trước node có giá trị lớn nhất
         public void InsertBeforeMax(int max)
         {
             IntNode pMax = GetMax();
@@ -239,7 +353,101 @@ namespace DSLKDon
             InsertBeforeP(pMax, pNew);
         }
 
+        //chèn X vào trước Y
+        public void InsertXBeforeY(int x, int y)
+        {
+            //5 -> 3 -> 7 -> 9
+            //chèn giá trị x = 10 vào trước y = 7
+            //5 -> 3 -> (10) -> 7 -> 9
+            IntNode pX = new IntNode(x);
+            IntNode p = first;
+            while (p != null)
+            {
+                if (p.Data == y)
+                {
+                    InsertAfterP(p, pX);
+                    HoanVi(p, pX);
+                    return;
+                }
+                p = p.Next;
+            }
+            Console.WriteLine("Khong tim thay node co gia tri y !");
+        }
 
+        //hàm dịch chuyển xoay vòng (RShiftRight)
+        public void RShiftRight()
+        {
+            if(IsEmpty() || Count == 1)
+            {
+                return; //Danh sách rỗng hoặc chỉ có 1 phần tử thì không dịch
+            }
 
+            last.Next = first; //Node cuối trỏ đến node đầu
+            first = last; //Node đầu trở thành node cuối
+            IntNode p = first;
+
+            while (p.Next != last)
+            {
+                p = p.Next;
+            }
+            last = p;
+            last.Next = null;
+        }
+
+        //phương thức sắp xếp theo thứ tự tăng dần(InterchangeSort)
+        public void InterchangeSort()
+        {
+            IntNode p = first;
+
+            while(p != null) //lặp qua toàn bộ danh sách
+            {
+                IntNode min = p; //giả sử node p là node có giá trị nhỏ nhất 
+                IntNode compare = p.Next; //bắt đầu so sánh với các node tiếp theo
+
+                while (compare != null) 
+                {
+                    if(compare.Data < min.Data) //so sánh GIÁ TRỊ của node so sánh(compare) với GIÁ TRỊ node nhỏ nhất(min)
+                    {
+                        min = compare; //cập nhật thành node nhỏ nhất
+                    }
+                    compare = compare.Next;
+                }
+
+                //Đổi chỗ giữa p và min(hoán đổi GIÁ TRỊ 2 node)
+                int temp = p.Data;
+                p.Data = min.Data;
+                min.Data = temp;    
+
+                p = p.Next; //di chuyển đến node tiếp theo để tiếp tục sắp xếp
+            }
+        }
+
+        //phương thức sắp xếp danh sách theo thứ tự giảm dần(SelectionSort)
+        public void SelectionSort()
+        {
+            IntNode p = first;
+
+            while(p != null)
+            {
+                IntNode max = p; //giả sử node hiện tại là node có giá trị nhỏ nhất
+                IntNode compare = p.Next; //bắt đầu so sánh với các node tiếp theo 
+
+                while (compare != null)
+                {
+                    if(compare.Data > max.Data) //so sánh GIÁ TRỊ của node so sánh(compare) với GIÁ TRỊ node lớn nhất(max)
+                    {
+                        max = compare;
+                    }
+                    compare= compare.Next;
+                }
+
+                //Đổi chỗ giữa p và max(hoán đổi GIÁ TRỊ 2 node)
+                int temp = p.Data;
+                p.Data = max.Data;
+                max.Data = temp;
+
+                p = p.Next; //di chuyển đến node tiếp theo để tiếp tục sắp xếp
+            }
+        }
     }
 }
--- DSLKDon/DSLKDon/Program.cs
+++ DSLKDon/DSLKDon/Program.cs
@@ -20,9 +20,9 @@ namespace DSLKDon
             Console.WriteLine("So luong phan tu trong danh sach la: " + sophantu);
 
             Console.Write("Tim gia tri: ");
-            int searchValue = int.Parse(Console.ReadLine());            
+            int searchValue = int.Parse(Console.ReadLine());
             IntNode resultNode = list.SearchX(searchValue);
-            if(resultNode != null)
+            if (resultNode != null)
             {
                 Console.WriteLine("Da tim thay duoc node co gia tri X can tim la " + resultNode.Data + " trong danh sach");
             }
@@ -43,18 +43,50 @@ namespace DSLKDon
             oddlist.ShowList();
 
             int min;
-            Console.Write("Nhap gia tri can chen: ");
+            Console.Write("Nhap gia tri can chen sau Min: ");
             int.TryParse(Console.ReadLine(), out min);
             list.InsertAfterMin(min);
             Console.WriteLine("DSLK sau khi chen la: ");
             list.ShowList();
 
             int max;
-            Console.Write("Nhap gia tri can chen: ");
+            Console.Write("Nhap gia tri can chen truoc Max: ");
             int.TryParse(Console.ReadLine(), out max);
             list.InsertBeforeMax(max);
             Console.WriteLine("DSLK sau khi chen la: ");
             list.ShowList();
+
+
+            int x1;
+            Console.Write("Nhap gia tri can xoa: ");
+            int.TryParse(Console.ReadLine(), out x1);
+            list.RemoveX(x1);
+            Console.WriteLine("DSLK sau khi xoa la: ");
+            list.ShowList();
+
+            int x;
+            int y;
+            Console.Write("Nhap gia tri X can chen: ");
+            int.TryParse(Console.ReadLine(), out x);
+            Console.Write("Nhap gia tri Y: ");
+            int.TryParse(Console.ReadLine(), out y);
+            list.InsertXAfterY(x, y);
+            //list.InsertXBeforeY(x, y);
+            Console.WriteLine("DSLK sau khi chen la: ");
+            list.ShowList();
+
+            list.RShiftRight();
+            Console.WriteLine("Danh sach sau khi dich phai xoay vong: ");
+            list.ShowList();
+
+            list.InterchangeSort();
+            Console.WriteLine("Danh sach sau khi sap xep tang dan(InterchangeSort): ");
+            list.ShowList();
+
+            list.SelectionSort();
+            Console.WriteLine("Danh sach sau khi sap xep giam dan(SelectionSort): ");
+            list.ShowList();
+
         }
         static void Main(string[] args)
         {
